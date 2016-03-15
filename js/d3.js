3cb490f4f6f0f2bb6b045dac5a75bec816a3e168
chihaoyo <chihaoyo@gmail.com>
2016-03-15 22:59:14 +0800

    使徒襲來 update

--- js/d3.js
+++ js/d3.js
@@ -38,6 +38,25 @@ var drawChart = function() {
 			'height': ch,
 			'viewBox': '0 0 ' + cw + ' ' + ch
 		});
+	svg.selectAll('g.axis').data(data.axes).enter().append('g')
+		.classed('axis', true)
+		.each(function(d, i) {
+			var g = d3.select(this);
+			g.append('line')
+				.attr({
+					'x1': 0,
+					'y1': yScale(i),
+					'x2': cw,
+					'y2': yScale(i),
+					'stroke': '#ccc',
+					'stroke-width': 2,
+				});
+			g.append('text').text(d)
+				.attr({
+					'x': 0,
+					'y': yScale(i) - 4,
+				})
+		});
 	svg.selectAll('g.proposal').data(data.proposals).enter().append('g')
 		.classed('proposal', true)
 		.attr('id', function(d) { return d.name; })
@@ -50,7 +69,7 @@ var drawChart = function() {
 					'cy': 40,
 					'r': 15,
 					'stroke': color,
-					'stroke-width': 5,
+					'stroke-width': 8,
 					'fill': '#ccc',
 				});
 			g.append('text')
@@ -65,13 +84,13 @@ var drawChart = function() {
 					'd': line(d.nodes), // http://www.oxxostudio.tw/articles/201411/svg-d3-02-line.html
 					'y': 0,
 					'stroke': color,
-					'stroke-width': 5,
+					'stroke-width': 8,
 					'fill': 'none',
 				});
 			g.selectAll('circle.value').data(d.values).enter().append('circle')
 				.classed('value', true)
 				.attr({
-					'r': 5,
+					'r': 8,
 					'cx': (function(d) { return xScale(d); }),
 					'cy': (function(d, i) { return yScale(i); }),
 					'fill': color,
@@ -88,24 +107,28 @@ var drawChart = function() {
 
 var drawTimelines = function() {
 	// data pre-processing
-	var dates = data.events.map(function(o) { return new Date(o.time); }).sort(function(a, b) { return a - b; });
-	var months = Math.round((dates[dates.length - 1] - dates[0])/1000/(30*24*60*60));
-	console.log(months, 'months');
-	var step = 120;
+	data.events = data.events.sort(function(a, b) { return new Date(a.time) - new Date(b.time)});
+
+	var dates = data.events.map(function(o) { return new Date(o.time); });//.sort(function(a, b) { return a - b; });
+	var firstDay = new Date(dates[0].getFullYear(), dates[0].getMonth(), 1);
+	//var months = Math.round((dates[dates.length - 1] - dates[0])/1000/(30*24*60*60));
+	var lastDay = new Date(new Date(dates[dates.length - 1].getFullYear(), (dates[dates.length - 1].getMonth()) + 1, 1) - 1);
+	//var step = 120;
 	var timelineDict = {};
 	for(var i = 0; i < data.timelines.length; i++) {
 		var id = data.timelines[i].id;
 		timelineDict[id] = data.timelines[i];
 		timelineDict[id].num = i;
 	};
+	console.log(timelineDict);
 
 	// now let us draw
 	var cw = ww;
-	var ch = months*step;
 	var paddings = {top: 40, right: Math.round(cw/6), bottom: 40, left: Math.round(cw/6)};
+	var ch = (lastDay - firstDay)/1000*0.00005 + paddings.top + paddings.bottom;//months*step;
 
 	var xScale = d3.scale.linear().domain([0, 2]).range([0 + paddings.left, cw - paddings.right]);
-	var yScale = d3.scale.linear().domain([dates[0].getTime()/1000, dates[dates.length - 1].getTime()/1000]).range([0 + paddings.top, ch - paddings.bottom]);
+	var yScale = d3.scale.linear().domain([firstDay.getTime()/1000, lastDay.getTime()/1000]).range([0 + paddings.top, ch - paddings.bottom]);
 	var colors = d3.scale.category10();
 
 	var svg = d3.select('#timelines').append('svg')
@@ -114,16 +137,30 @@ var drawTimelines = function() {
 			'height': ch,
 			'viewBox': '0 0 ' + cw + ' ' + ch,
 		});
-	for(var i = 1; i < months; i++) {
-		svg.append('circle').attr({'cx': cw/2, 'cy': i*step, 'r': 3, 'fill': 'rgba(0, 0, 0, 0.5)'});
+	for(var i = firstDay; i < lastDay; i = new Date(i.setMonth(i.getMonth() + 1))) {
+		svg.append('circle').attr({
+			'cx': cw/2,
+			'cy': yScale(i.getTime()/1000),
+			'r': 3,
+			'fill': 'rgba(0, 0, 0, 0.35)',
+		});
 	}
+	svg.append('circle').attr({
+		'cx': cw/2,
+		'cy': yScale(lastDay.getTime()/1000),
+		'r': 3,
+		'fill': 'rgba(0, 0, 0, 0.35)'
+	});
+
+	var last = {x: 0, y: 0};
 	svg.selectAll('circle.event').data(data.events).enter().append('g')
 		.classed('event', true)
 		.each(function(d, i) {
 			var g = d3.select(this);
+			var r = 15, s = 8, fontSize = 12;
 			var x = xScale(timelineDict[d.timeline].num);
 			var y = yScale((new Date(d.time)).getTime()/1000);
-			var r = 8;
+			if(y - last.y < 2*r && x == last.x) x += 2*r;
 			var color = timelineDict[d.timeline].color;
 			g.append('circle')
 				.classed('event', true)
@@ -131,19 +168,17 @@ var drawTimelines = function() {
 					'cx': x,
 					'cy': y,
 					'r': r,
-					'fill': color,
+					'fill': '#ccc',
+					'stroke': color,
+					'stroke-width': s,
 				});
-			g.append('text')
+			/*g.append('text')
 				.text(d.time)
 				.attr({
 					'x': x,
-					'y': y + r/2,
+					'y': y + r + s + fontSize,
 					'text-anchor': 'middle',
-				})
+				});*/
+			last = {x: x, y: y};
 		});
 };
-$(function() {
-	ww = $('body').width();
-	drawChart();
-	drawTimelines();
-});
--- js/index.js
+++ js/index.js
@@ -6,10 +6,11 @@ $(function() {
 		$('<h2>').text(data.chapters[i].title).appendTo($card);
 		$('<p>').addClass('description').text(data.chapters[i].description).appendTo($card);
 		var $expand = $('<div>').addClass('expand').appendTo($chapter);
-		var $expandButton = $('<button>').text('展開').appendTo($expand).click(function() {
+		var expandText = {expand: '放大', collapse: '縮小'};
+		var $expandButton = $('<button>').text(expandText.expand).appendTo($expand).click(function() {
 			var $b = $(this);
 			$b.parents('.chapter').toggleClass('expanded');
-			$b.text(($b.text() == '展開' ? '收合' : '展開'));
+			$b.text(($b.text() == expandText.expand ? expandText.collapse : expandText.expand));
 		});
 		var $reports = $('<div>').addClass('reports').appendTo($chapter);
 		for(var j = 0; j < data.reports.length; j++) {
@@ -28,14 +29,18 @@ $(function() {
 	}
 
 	var $legislature = $('#legislature');
-	var $timelineTitles = $('#timelineTitles');
+/*	var $timelineTitles = $('#timelineTitles');
 	for(var i = 0; i < data.timelines.length; i++) {
 		$('<div>').addClass('title').appendTo($timelineTitles)
 			.text(data.timelines[i].title)
 			.css('background-color', data.timelines[i].color);
-	}
+	}*/
 
 	$('nav > ul > li').click(function() {
 		$('body').animate({'scrollTop': $('section#' + $(this).attr('data-target')).position().top - 88 + 1});
-	})
+	});
+
+	ww = $('body').width();
+	drawChart();
+	drawTimelines();
 });
